45df0e2814955269d84c6ee239ec6d0e816ef20c
Venu4i <venuverma04@gmail.com>
2025-01-23 18:34:38 +0530

    commiting photo

--- Webdemo/src/components/Home/Home.jsx
+++ Webdemo/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import me from '../../../src/me.jpg' ;
 
 function Home(){
     const [clicked, setClicked] = useState(false);
@@ -16,7 +17,7 @@ function Home(){
         <>
         <div className="max-w-screen-2xl container mx-auto px-4 pmd:px-20 py-20 my-20 "> 
            <div className="flex flex-col md:flex-row">
-              <div className="md:w-1/2">
+              <div className="md:w-1/2 md:text-3xl md:px-20">
                  <span> Hello!</span>
                  <h1>I am Venu Verma</h1>
                  <p > Registraton Number: 20243316  </p>
@@ -26,8 +27,8 @@ function Home(){
      onClick={Click} >My instagram  </button >
                 
                     </div>
-              <div>
-                <img className="rounded-full md:h-450 md:ml-40 md:mt-16 "  src="" alt="Display picture" />
+              <div className="md:ml-40 md:mt-16">
+                <img className="rounded-full md:h-[450px] w-[450px]  "  src={me} alt="Display picture" />
               </div>
             </div> 
         </div>
